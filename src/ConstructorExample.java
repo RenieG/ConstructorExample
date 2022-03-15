f74f07119c4012cd1590479a14378975f0bd98f8
RenieG <igakaho@yahoo.com>
2022-03-15 13:08:05 -0500

    Initial commit

---
+++ src/ConstructorExample.java
@@ -0,0 +1,41 @@
+public class ConstructorExample {
+
+    public static void main(String[] args) {
+
+    }
+
+    String name;
+    int Age;
+    String schoolName;
+
+    public ConstructorExample(String n, int a, String sName) {
+        name = n;
+        Age = a;
+        schoolName = sName;
+
+    }
+
+
+    public String toString() {
+        return ("Hello" + "" + name + "" + "Age:" + "" + Age + "" + "schoolName:" + "" + schoolName);
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/ConstructorTester.java
@@ -0,0 +1,9 @@
+public class ConstructorTester {
+
+
+    public static void main(String []args){
+        ConstructorExample newStudent = new ConstructorExample ("Irene", 44, "DeVry");
+
+        System.out.println(newStudent);
+    }
+}
